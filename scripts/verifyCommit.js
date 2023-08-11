928be22a304ba842875e88f3fe592cb0c234b0be
oyjt <oyjt001@gmail.com>
2023-08-12 00:20:59 +0800

    build: 代码提交规范优化

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import { readFileSync } from 'fs'
-import chalk from 'chalk'
+import pc from 'picocolors'
 
 const msgPath = path.resolve('.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
@@ -11,9 +11,9 @@ const commitRE =
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+    `  ${pc.bgRed(pc.white(' ERROR '))} ${pc.red(
       'Git提交信息不符合 Angular 规范!'
-    )}\n\n${chalk.green('推荐使用命令 npm run cz 生成符合规范的Git提交信息')}\n`
+    )}\n\n${pc.green('推荐使用命令 npm run cz 生成符合规范的Git提交信息')}\n`
   )
   process.exit(1)
 }
