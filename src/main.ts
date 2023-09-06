11c5c5f815af0fbf8059cf20fcc6237e61a31749
oyjt <oyjt001@gmail.com>
2023-09-07 01:21:01 +0800

    style: 格式调整

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 // 引入全局uview-plus
-import uviewPlus from 'uview-plus'
-import { createSSRApp } from 'vue'
-import App from '@/App.vue'
-import store from '@/store'
+import uviewPlus from 'uview-plus';
+import { createSSRApp } from 'vue';
+import App from '@/App.vue';
+import store from '@/store';
 
 // 引入mixin封装
-import initMixin from '@/common/mixin'
+import initMixin from '@/common/mixin';
 
 // 引入请求封装
-import initRequest from '@/utils/request/index'
+import initRequest from '@/utils/request/index';
 
 // http接口API抽离，免于写url或者一些固定的参数
-import initApi from '@/api/index'
+import initApi from '@/api/index';
 
 // #ifdef VUE3
 export function createApp() {
-  const app = createSSRApp(App)
-  app.use(uviewPlus)
-  app.use(store)
+  const app = createSSRApp(App);
+  app.use(uviewPlus);
+  app.use(store);
 
-  initRequest(app)
-  initMixin(app)
-  initApi(app)
+  initRequest(app);
+  initMixin(app);
+  initApi(app);
 
   return {
     app
-  }
+  };
 }
 // #endif
