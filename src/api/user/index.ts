39b3e41a3f10b245eff0e0555734c075c0c4a550
oyjt <oyjt001@gmail.com>
2023-09-07 01:16:46 +0800

    style: 格式整理

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,8 +1,8 @@
 /**
  * 用户信息相关接口
  */
-import { get, post } from '@/utils/request'
-import type { UserState } from '@/store/modules/user/types'
+import { get, post } from '@/utils/request';
+import type { UserState } from '@/store/modules/user/types';
 
 enum URL {
   login = '/user/login',
@@ -10,14 +10,14 @@ enum URL {
   profile = '/user/profile'
 }
 interface LoginRes {
-  token: string
+  token: string;
 }
 
-interface LoginData {
-  username: string
-  password: string
+export interface LoginData {
+  username: string;
+  password: string;
 }
 
-export const getUserProfile = () => get<UserState>({ url: URL.profile })
-export const login = (data: LoginData) => post<any>({ url: URL.login, data })
-export const logout = () => post<LoginRes>({ url: URL.logout })
+export const getUserProfile = () => get<UserState>({ url: URL.profile });
+export const login = (data: LoginData) => post<any>({ url: URL.login, data });
+export const logout = () => post<LoginRes>({ url: URL.logout });
