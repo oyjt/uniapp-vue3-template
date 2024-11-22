74573e90dbe23247654fa6467f213df1a6716129
江阳小道 <oyjt001@gmail.com>
2024-11-23 02:49:52 +0800

    feat: 集成alova请求库

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,54 +1,131 @@
-// 引入配置
-import type { HttpRequestConfig, HttpResponse } from 'uview-plus/libs/luch-request/index';
 import type { IResponse } from './type';
-import Request from 'uview-plus/libs/luch-request/index';
-import { requestInterceptors, responseInterceptors } from './interceptors';
+import { getToken } from '@/utils/auth';
+import type { uniappRequestAdapter } from '@alova/adapter-uniapp';
+import AdapterUniapp from '@alova/adapter-uniapp';
+import { createAlova } from 'alova';
+import { showMessage } from './status';
+import VueHook from 'alova/vue';
+import { createServerTokenAuthentication } from 'alova/client';
+import { useUserStore } from '@/store';
 
-const http = new Request();
+/**
+ * @description: 请求结果设置
+ */
+export enum ResultEnum {
+  SUCCESS = 10000,
+  FAIL = 10001,
+  ERROR = 1,
+  TIMEOUT = 401,
+  TYPE = 'success',
+}
 
-// 引入拦截器配置
-export function setupRequest() {
-  http.setConfig((defaultConfig: HttpRequestConfig) => {
-    /* defaultConfig 为默认全局配置 */
-    defaultConfig.baseURL = import.meta.env.VITE_API_BASE_URL;
-    // #ifdef H5
-    if (import.meta.env.VITE_APP_PROXY === 'true') {
-      defaultConfig.baseURL = import.meta.env.VITE_API_PREFIX;
+/**
+ * @description: contentType
+ */
+export enum ContentTypeEnum {
+  // json
+  JSON = 'application/json;charset=UTF-8',
+  // form-data qs
+  FORM_URLENCODED = 'application/x-www-form-urlencoded;charset=UTF-8',
+  // form-data  upload
+  FORM_DATA = 'multipart/form-data;charset=UTF-8',
+}
+
+const ContentType = {
+  'Content-Type': ContentTypeEnum.JSON,
+  'Accept': 'application/json, text/plain, */*',
+};
+const { onAuthRequired, onResponseRefreshToken } = createServerTokenAuthentication<typeof VueHook, typeof uniappRequestAdapter>({
+  refreshTokenOnError: {
+    // 响应时触发，可获取到error和method，并返回boolean表示token是否过期
+    // 当服务端返回401时，表示token过期
+    isExpired: (error) => {
+      return error.response.status === 401;
+    },
+
+    // 当token过期时触发，在此函数中触发刷新token
+    handler: async () => {
+      try {
+        await useUserStore().authLogin();
+      }
+      catch (error) {
+        // token刷新失败，跳转回登录页
+        uni.reLaunch({ url: '/pages/common/login/index' });
+        // 并抛出错误
+        throw error;
+      }
+    },
+  },
+});
+
+/**
+ * alova 请求实例
+ * @link https://github.com/alovajs/alova
+ */
+const alovaInstance = createAlova({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  ...AdapterUniapp(),
+  timeout: 5000,
+  statesHook: VueHook,
+  beforeRequest: onAuthRequired((method) => {
+    method.config.headers = Object.assign(method.config.headers, ContentType);
+    const { config } = method;
+    const ignoreAuth = !config.meta?.ignoreAuth;
+    const authorization = ignoreAuth ? getToken() : null;
+    if (ignoreAuth && !authorization) {
+      throw new Error('[请求错误]：未登录');
     }
-    // #endif
-    return defaultConfig;
+    method.config.headers.token = getToken();
+  }),
+  responded: onResponseRefreshToken((response, method) => {
+    const { config } = method;
+    const { requestType } = config;
+    const { statusCode, data: rawData, errMsg } = response as UniNamespace.RequestSuccessCallbackResult;
+    if (statusCode === 200) {
+      if (requestType) {
+        return response;
+      }
+      const { code, message, result } = rawData as IResponse;
+      if (code === ResultEnum.SUCCESS) {
+        return result as any;
+      }
+
+      // 如果没有显式定义custom的toast参数为false的话，默认对报错进行toast弹出提示
+      if (config.meta?.toast !== false)
+        uni.$u.toast(message);
+      throw new Error(`请求错误[${code}]：${message}`);
+    }
+    // 处理http状态错误
+    setTimeout(() => {
+      uni.$u.toast(showMessage(statusCode));
+    }, 500);
+    throw new Error(`HTTP请求错误[${statusCode}]：${errMsg}`);
+  }),
+});
+
+export const request = alovaInstance;
+
+export function get<T>({ url = '', params = {}, config = {} }): Promise<T> {
+  return request.Get<T>(url, { ...params, ...config });
+}
+
+export function post<T>({ url = '', data = {}, config = {} }): Promise<T> {
+  return request.Post<T>(url, data, config);
+}
+
+export function upload<T>({ url = '', data = {}, config = {} }): Promise<T> {
+  return request.Post<T>(url, data, {
+    // 设置请求方式为上传，适配器内将调用uni.uploadFile
+    requestType: 'upload',
+    ...config,
   });
-  requestInterceptors(http);
-  responseInterceptors(http);
 }
 
-export function request<T = any>(config: HttpRequestConfig): Promise<T> {
-  return new Promise((resolve, reject) => {
-    http.request(config).then((res: HttpResponse<IResponse<T>>) => {
-      console.log('[ res ] >', res);
-      const { result } = res.data;
-      resolve(result as T);
-    }).catch((err: any) => {
-      console.error('[ err ] >', err);
-      reject(err);
-    });
+export function download<T>({ url = '', params = {}, config = {} }): Promise<T> {
+  return request.Get<T>(url, {
+    // 设置请求方式为下载，适配器内将调用uni.downloadFile
+    requestType: 'download',
+    ...params,
+    ...config,
   });
 }
-
-export function get<T = any>(config: HttpRequestConfig): Promise<T> {
-  return request({ ...config, method: 'GET' });
-}
-
-export function post<T = any>(config: HttpRequestConfig): Promise<T> {
-  return request({ ...config, method: 'POST' });
-}
-
-export function upload<T = any>(config: HttpRequestConfig): Promise<T> {
-  return request({ ...config, method: 'UPLOAD' });
-}
-
-export function download<T = any>(config: HttpRequestConfig): Promise<T> {
-  return request({ ...config, method: 'DOWNLOAD' });
-}
-
-export default setupRequest;
