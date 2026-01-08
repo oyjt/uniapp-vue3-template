871471eb54b25e6d92c8763eec43510012caf568
oyjt <owen@udataai.com>
2026-01-08 17:08:52 +0800

    chore: 依赖升级

--- src/store/modules/app/index.ts
+++ src/store/modules/app/index.ts
@@ -41,34 +41,6 @@ const useAppStore = defineStore('app', {
       // 保存到本地存储
       storage.set(THEME_KEY, this.theme);
     },
-    checkUpdate() {
-      const updateManager = uni.getUpdateManager();
-      updateManager.onCheckForUpdate((res: UniApp.OnCheckForUpdateResult) => {
-        // 请求完新版本信息的回调
-
-        console.log(res.hasUpdate);
-      });
-      updateManager.onUpdateReady(() => {
-        uni.showModal({
-          title: '更新提示',
-          content: '新版本已经准备好，是否重启应用?',
-          success(res) {
-            if (res.confirm) {
-              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-              updateManager.applyUpdate();
-            }
-          },
-        });
-      });
-      updateManager.onUpdateFailed((res: any) => {
-        console.error(res);
-        // 新的版本下载失败
-        uni.showToast({
-          title: '更新失败',
-          icon: 'error',
-        });
-      });
-    },
   },
 });
 
